cef754d3e0d84b88f04b58491b58f9f06bb770b1
Edu58 <edumuriithi58@gmail.com>
2023-02-03 21:46:47 +0300

    feat: setup drf-yasg API documentation

--- APO-Backend/APO_Store/settings.py
+++ APO-Backend/APO_Store/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
+    # Packages
+    "drf_yasg",
+
+    # Apps
     "Users",
 ]
 
--- APO-Backend/APO_Store/urls.py
+++ APO-Backend/APO_Store/urls.py
@@ -14,8 +14,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="APO Store API",
+        default_version="v1",
+        description="This API is created and maintained by APO Store developers. It provides a way for other "
+                    "developers to extend certain APO Store functionality to other software's e.g. mobile, web",
+        terms_of_service="",
+        contact=openapi.Contact(email="edumuriithi58@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=False,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^api/v1/(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^api/v1/docs/swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^api/v1/docs/redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
