652d25aca446cb5683cc5385aa3e49cde4bced9c
Edu58 <edumuriithi58@gmail.com>
2023-02-07 22:52:21 +0300

    feat: register Product, ProductCategory and Discount models

--- APO-Backend/Catalogue/admin.py
+++ APO-Backend/Catalogue/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from .models import ProductCategory
+from .models import ProductCategory, Discount, Product
 
 # Register your models here.
-admin.site.register(ProductCategory)
+admin.site.register([
+    ProductCategory,
+    Discount,
+    Product
+])
